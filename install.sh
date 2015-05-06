c361943e8f79e84405224cba84068859324282fa
justin talbott <justin@waymondo.com>
2015-05-06 00:38:48 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 printf " Installing Emacs"
-/usr/bin/env brew install emacs --cocoa --srgb --with-glib --with-imagemagick --with-rsvg --HEAD
+/usr/bin/env brew install emacs --with-cocoa --with-glib --with-imagemagick --with-rsvg --HEAD
 /usr/bin/env brew linkapps emacs
 printf " Cloning Hemacs"
 /usr/bin/env git clone https://github.com/waymondo/hemacs.git "$HOME/.emacs.d"
 printf " Installing Hemacs"
+/usr/bin/env cd "$HOME/.emacs.d"
+/usr/bin/env git submodule update --recursive
