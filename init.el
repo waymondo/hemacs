ca1828b0fad4c94cc6e158160fdf4a85b0baba65
justin talbott <justin@waymondo.com>
2016-08-18 12:27:00 -0400

    don't show garbage collection messages

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
       ns-right-option-modifier 'none
       create-lockfiles nil
       mouse-wheel-scroll-amount '(1 ((shift) . 1))
-      garbage-collection-messages t
       gc-cons-threshold (* 10 1024 1024)
       disabled-command-function nil
       ad-redefinition-action 'accept
