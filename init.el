739553a40c50e3a5e00c756dba23c15310d2dba7
justin talbott <justin@waymondo.com>
2018-02-21 12:29:54 -0500

    add smerge hydra for a nice conflict merging interface
    again ripped from doom-emacs

--- init.el
+++ init.el
@@ -1470,6 +1470,52 @@
   (def github-new-issue
     (github-issues "new")))
 
+(use-package smerge-mode
+  :after hydra
+  :hook
+  (find-file . enable-smerge-mode-maybe)
+  :bind
+  (:map smerge-mode-map ("s-M" . hydra-smerge/body))
+  :config
+  (defun enable-smerge-mode-maybe ()
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "^<<<<<<< " nil :noerror)
+        (smerge-mode 1)
+        (hydra-smerge/body))))
+  (defhydra hydra-smerge (:hint nil :pre (smerge-mode 1) :post (smerge-auto-leave))
+    "
+                                                    ╭────────┐
+  Movement   Keep           Diff              Other │ smerge │
+  ╭─────────────────────────────────────────────────┴────────╯
+     ^_g_^       [_b_] base       [_<_] upper/base    [_C_] Combine
+     ^_C-k_^     [_u_] upper      [_=_] upper/lower   [_r_] resolve
+     ^_k_ ↑^     [_l_] lower      [_>_] base/lower    [_R_] remove
+     ^_j_ ↓^     [_a_] all        [_H_] hightlight
+     ^_C-j_^     [_RET_] current  [_E_] ediff             ╭──────────
+     ^_G_^                                            │ [_q_] quit"
+    ("g" (progn (goto-char (point-min)) (smerge-next)))
+    ("G" (progn (goto-char (point-max)) (smerge-prev)))
+    ("C-j" smerge-next)
+    ("C-k" smerge-prev)
+    ("j" next-line)
+    ("k" previous-line)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("H" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("R" smerge-kill-current)
+    ("q" nil :color blue)))
+
 (use-package git-timemachine)
 
 (use-package gist
