71c00ec62bb95985da5c373ff171593597e60520
justin talbott <justin@waymondo.com>
2022-08-16 22:05:55 -0400

    bump straight bootstrap version

--- early-init.el
+++ early-init.el
@@ -13,7 +13,7 @@
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
