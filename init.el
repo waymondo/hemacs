ac50edf6cc6f5f548a84cd9ce0f849474e47c02c
justin talbott <justin@waymondo.com>
2024-12-29 20:45:07 -0500

    bind expreg contract, change embark-act binding
    and which-key is built-in now, so no need for after

--- init.el
+++ init.el
@@ -299,7 +299,8 @@
 
 (use-package expreg
   :bind
-  ("C-," . expreg-expand))
+  ("C-," . expreg-expand)
+  ("C-." . expreg-contract))
 
 (use-feature subword
   :hook
@@ -475,38 +476,37 @@
   :custom
   (prefix-help-command #'embark-prefix-help-command)
   :bind
-  ("C->" . embark-act)
+  ("C-;" . embark-act)
   ([remap describe-bindings] . embark-bindings)
   :config
-  (after which-key
-    (defun embark-which-key-indicator ()
-      (lambda (&optional keymap targets prefix)
-        (if (null keymap)
-            (which-key--hide-popup-ignore-command)
-          (which-key--show-keymap
-           (if (eq (plist-get (car targets) :type) 'embark-become)
-               "Become"
-             (format "Act on %s '%s'%s"
-                     (plist-get (car targets) :type)
-                     (embark--truncate-target (plist-get (car targets) :target))
-                     (if (cdr targets) "…" "")))
-           (if prefix
-               (pcase (lookup-key keymap prefix 'accept-default)
-                 ((and (pred keymapp) km) km)
-                 (_ (key-binding prefix 'accept-default)))
-             keymap)
-           nil nil t (lambda (binding)
-                       (not (string-suffix-p "-argument" (cdr binding))))))))
-    (setopt embark-indicators
-            '(embark-which-key-indicator
-              embark-highlight-indicator
-              embark-isearch-highlight-indicator))
-    (defun embark-hide-which-key-indicator (fn &rest args)
-      (which-key--hide-popup-ignore-command)
-      (let ((embark-indicators
-             (remq #'embark-which-key-indicator embark-indicators)))
-        (apply fn args)))
-    (advice-add #'embark-completing-read-prompter :around #'embark-hide-which-key-indicator)))
+  (defun embark-which-key-indicator ()
+    (lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+        (which-key--show-keymap
+         (if (eq (plist-get (car targets) :type) 'embark-become)
+             "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets) :target))
+                   (if (cdr targets) "…" "")))
+         (if prefix
+             (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+         nil nil t (lambda (binding)
+                     (not (string-suffix-p "-argument" (cdr binding))))))))
+  (setopt embark-indicators
+          '(embark-which-key-indicator
+            embark-highlight-indicator
+            embark-isearch-highlight-indicator))
+  (defun embark-hide-which-key-indicator (fn &rest args)
+    (which-key--hide-popup-ignore-command)
+    (let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+  (advice-add #'embark-completing-read-prompter :around #'embark-hide-which-key-indicator))
 
 (use-package embark-consult
   :demand t
