9e2e380111e6e46393909669963a15180fedb938
justin talbott <justin@waymondo.com>
2025-03-15 12:51:28 -0400

    markdown-ts-mode built-in now

--- init.el
+++ init.el
@@ -726,12 +726,7 @@
   :hook
   (sgml-mode web-mode))
 
-(use-package markdown-ts-mode
-  :mode
-  ("\\.md\\'" . markdown-ts-mode)
-  :config
-  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
-  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
+(use-feature markdown-ts-mode)
 
 (use-package gh-md
   :after markdown-ts-mode
